1ce5ae69492e11929a851696839b99c2ddac9e14
capradiomars <marshawn.davidson@capradio.org>
2025-06-10 13:28:29 -0700

    chart edits

--- audience chart.js
+++ audience chart.js
@@ -38,7 +38,7 @@ function showTab(index) {
     const stats = tabData[0].stats;
     content.innerHTML = `
       <div class="max-w-[585px] mb-[24px] md:mb-[32px]">
-        <p class="h-[90px] text-[18px] leading-[24px]">
+        <p class="text-[18px] leading-[24px] mt-[16px] sm:mt-0 md:h-[90px]">
           CapRadio listeners are more than just listeners. They’re curious, engaged and action-oriented choosing CapRadio to stay informed, involved and inspired. Your message reaches them across platforms and across the region with trusted content that earns attention.
         </p>
       </div>
@@ -65,19 +65,19 @@ function showTab(index) {
 
   content.innerHTML = `
     <div class="leading-[21px] grid content-center grid-cols-1 md:grid-cols-2 gap-[16px]">
-      <div class="bg-gray-100 grid gap-[8px] justify-items-center content-center p-[16px] rounded-[40px]">
+      <div class="bg-gray-100 grid gap-[8px] justify-items-center content-center p-[24px] rounded-[40px] max-w-[329px]">
         <svg class="w-[61px] fill-[#9E2A2FCC]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 616 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M602 118.6L537.1 15C531.3 5.7 521 0 510 0H106C95 0 84.7 5.7 78.9 15L14 118.6c-33.5 53.5-3.8 127.9 58.8 136.4 4.5 .6 9.1 .9 13.7 .9 29.6 0 55.8-13 73.8-33.1 18 20.1 44.3 33.1 73.8 33.1 29.6 0 55.8-13 73.8-33.1 18 20.1 44.3 33.1 73.8 33.1 29.6 0 55.8-13 73.8-33.1 18.1 20.1 44.3 33.1 73.8 33.1 4.7 0 9.2-.3 13.7-.9 62.8-8.4 92.6-82.8 59-136.4zM529.5 288c-10 0-19.9-1.5-29.5-3.8V384H116v-99.8c-9.6 2.2-19.5 3.8-29.5 3.8-6 0-12.1-.4-18-1.2-5.6-.8-11.1-2.1-16.4-3.6V480c0 17.7 14.3 32 32 32h448c17.7 0 32-14.3 32-32V283.2c-5.4 1.6-10.8 2.9-16.4 3.6-6.1 .8-12.1 1.2-18.2 1.2z"/></svg>
         <p class="text-center text-[#5C0000]">CapRadio ${index === 1 ? "news" : "music"} listeners spend over <strong>$2.5 billion annually</strong> in metro area retail sales</p>
       </div>
-      <div class="bg-gray-100 grid justify-center p-[16px] rounded-[40px] h-[240px]">
+      <div class="bg-gray-100 grid justify-center p-[16px] rounded-[40px] max-w-[329px] h-[240px]">
         <h5 class="text-center relative top-[32px]">GENDER</h5>
         <canvas id="genderChart"></canvas>
       </div>
-      <div class="bg-gray-100 grid justify-center p-[16px] rounded-[40px] h-[300px]">
+      <div class="bg-gray-100 grid justify-center p-[16px] rounded-[40px] max-w-[329px] h-[300px]">
         <h5 class="text-center relative top-[28px]">AGE RANGES</h5>
         <canvas id="ageChart"></canvas>
       </div>
-      <div class="bg-gray-100 grid justify-center p-[16px] rounded-[40px] h-[300px]">
+      <div class="bg-gray-100 grid justify-center p-[16px] rounded-[40px] max-w-[329px] h-[300px]">
         <h5 class="text-center relative top-[28px]">HOUSEHOLD INCOME</h5>
         <canvas id="incomeChart"></canvas>
       </div>
